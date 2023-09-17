3f273043492ad8c0937e6c9ed2e3343468cd05ac
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-09-17 10:37:34 +0300

    Fixed events routes

--- backend/application_code/controllers/event_controller.py
+++ backend/application_code/controllers/event_controller.py
@@ -56,6 +56,7 @@ def get_events():
             events_list = [
                     {
                         'event_id': event.event_id,
+                        'organizer_id': event.organizer_id,
                         'event_name': event.event_name,
                         'event_date': event.event_date,
                         'event_time': event.event_time.strftime('%H:%M:%S'),
